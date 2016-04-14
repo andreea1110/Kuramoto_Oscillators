10ab790b750a0aff55ce2626e6ca8f5acca60a07
Andreea Ciuprina <andreea.c1110@gmail.com>
2016-04-15 01:41:30 +0200

    Improved corr mat plot

--- plotting_functions.py
+++ plotting_functions.py
@@ -38,7 +38,7 @@ def plot_time_evolution(sol, t, ax):
 	nf = np.size(sol, 0)
 	for i in range(1, nf):
 		ax.plot(t, sol[i, :], label = "f %d" % (i))
-	#ax.legend(loc = 'upper right', prop = {'size':7})
+	ax.legend(loc = 'upper right', prop = {'size':7})
 	ax.set_xlabel("t")
 	ax.set_ylabel(r"$\theta$")
 
@@ -53,6 +53,30 @@ def plot_heatmap_evolutions(sol, t, ax):
     ax.set_xlabel(r'$t$')
     ax.set_ylabel(r'$\Theta_i$')
 
+def plot_sum_corr_mat(sum_rho_avg, ax):
+	""" Plot the averaged sum of the correlation matrices
+	"""
+	im = ax.imshow(sum_rho_avg, 
+		cmap = plt.cm.coolwarm, 
+		interpolation='nearest')
+	divider = make_axes_locatable(ax)
+	cax = divider.append_axes('right', size='5%', pad=0.2)
+	cbar = plt.colorbar(im, cax=cax)
+	cbar.ax.tick_params(labelsize=10) 
+	ax.set_title('Correlation matrix', fontsize=10)
+
+def plot_time_to_sync(D_average, ax):
+	""" Plot the matrix stroring the time to correlation for one run
+	"""
+	im = plt.imshow(D_average, 
+		cmap = plt.cm.coolwarm, 
+		interpolation='nearest')
+	divider = make_axes_locatable(ax)
+	cax = divider.append_axes('right', size='5%', pad=0.2)
+	cbar = plt.colorbar(im, cax=cax)
+	cbar.ax.tick_params(labelsize=10) 
+	ax.set_title('Time to synchronization in DCM', fontsize=10)
+
 
 def plot_corr_mat_dcm(G, sum_rho_avg, D_average, no_runs):
 	""" Plot the sum of the correlation matrices, 
@@ -60,30 +84,9 @@ def plot_corr_mat_dcm(G, sum_rho_avg, D_average, no_runs):
 	"""
 	plt.figure()
 	gs = mpl.gridspec.GridSpec(1, 3)
-
-	# plot the network graphs
 	plot_graph(G, plt.subplot(gs[:, 0]))
-
-	# plot the averaged sum of the correlation matrices
-	ax = plt.subplot(gs[:, 1])
-	im = ax.imshow(sum_rho_avg, 
-		cmap = plt.cm.coolwarm, 
-		interpolation='nearest')
-	divider = make_axes_locatable(ax)
-	cax = divider.append_axes('right', size='5%', pad=0.2)
-	plt.colorbar(im, cax=cax)
-	ax.set_title('Correlation matrix')
-
-	# plot the time to correlation matrix
-	ax = plt.subplot(gs[:, 2])
-	plt.imshow(D_average, 
-		cmap = plt.cm.coolwarm, 
-		interpolation='nearest')
-	divider = make_axes_locatable(ax)
-	cax = divider.append_axes('right', size='5%', pad=0.2)
-	plt.colorbar(im, cax=cax)
-	ax.set_title('Time to synchronization in DCM')
-
+	plot_sum_corr_mat(sum_rho_avg, plt.subplot(gs[:, 1]))
+	plot_time_to_sync(D_average, plt.subplot(gs[:, 2]))
 	plt.suptitle('Average over %s runs' % (no_runs))
 	plt.savefig('images/corrMat_dcm.png')
 	plt.savefig('images/corrMat_dcm.pdf')
@@ -94,7 +97,6 @@ def plot_time_corr(G, sol, corr_vals, t):
 	"""
 	plt.figure()
 	gs = mpl.gridspec.GridSpec(2, 2)
-
 	plot_graph(G, plt.subplot(gs[0, 0]))
 	plot_time_evolution(sol, t, plt.subplot(gs[0, 1]))
 	plot_corr_vals(corr_vals, t, plt.subplot(gs[1, 1]))
