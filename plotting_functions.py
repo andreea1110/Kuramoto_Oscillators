caaf7766885040ab8cb4c7c7fef55e376587dedf
Andreea Ciuprina <andreea.c1110@gmail.com>
2016-04-15 00:22:11 +0200

    Add heat-map plot

--- plotting_functions.py
+++ plotting_functions.py
@@ -23,28 +23,36 @@ def plot_graph(graph, ax):
         ax=ax)
     nx.draw_networkx_labels(graph, pos, labels)
 
-def plot_corr_vals(corr_vals, t):
-	plt.figure()
+def plot_corr_vals(corr_vals, t, ax):
+	""" Plot the correlation values between each of the nodes, over time
+	"""
 	for i in range(np.size(corr_vals, 0)):
-		plt.plot(t, corr_vals[i, :])
-	plt.xlabel('t')
-	plt.ylabel('correlation values')
-	plt.savefig('images/corrValAvg.png')
-	plt.savefig('images/corrValAvg.pdf')
+		ax.plot(t, corr_vals[i, :])
+	ax.set_xlabel('t')
+	ax.set_ylabel('correlation values')
 
-def plot_time_evolution(sol, t):
+
+def plot_time_evolution(sol, t, ax):
 	""" Plot the time evolution of the system
 	"""
-	plt.figure()
 	nf = np.size(sol, 0)
-	for i in range(nf):
-		plt.plot(t, sol[i, :], label = "f %d" % (i))
-	plt.legend(loc = 'upper right', prop = {'size':7})
-	plt.xlabel("t")
-	plt.ylabel(r"$\theta$")
-	#plt.xlim(0, 10)
-	plt.savefig('images/timeSol.png')
-	plt.savefig('images/timeSol.pdf')
+	for i in range(1, nf):
+		ax.plot(t, sol[i, :], label = "f %d" % (i))
+	#ax.legend(loc = 'upper right', prop = {'size':7})
+	ax.set_xlabel("t")
+	ax.set_ylabel(r"$\theta$")
+
+def plot_heatmap_evolutions(sol, t, ax):
+    """ Plot system evolution as a heat map
+    """
+    ax.imshow(
+        sol, aspect='auto',
+        cmap=plt.cm.gray, interpolation='nearest', 
+        extent = (0, t[-1], 0, np.size(sol, 0)))
+
+    ax.set_xlabel(r'$t$')
+    ax.set_ylabel(r'$\Theta_i$')
+
 
 def plot_corr_mat_dcm(G, sum_rho_avg, D_average, no_runs):
 	""" Plot the sum of the correlation matrices, 
@@ -80,3 +88,17 @@ def plot_corr_mat_dcm(G, sum_rho_avg, D_average, no_runs):
 	plt.savefig('images/corrMat_dcm.png')
 	plt.savefig('images/corrMat_dcm.pdf')
 
+def plot_time_corr(G, sol, corr_vals, t):
+	""" Plot the time evolution (as time series and as heat-map), together
+		with the corresponding correlation values between each of the nodes
+	"""
+	plt.figure()
+	gs = mpl.gridspec.GridSpec(2, 2)
+
+	plot_graph(G, plt.subplot(gs[0, 0]))
+	plot_time_evolution(sol, t, plt.subplot(gs[0, 1]))
+	plot_corr_vals(corr_vals, t, plt.subplot(gs[1, 1]))
+	plot_heatmap_evolutions(sol, t, plt.subplot(gs[1, 0]))
+	plt.savefig('images/time_corr.png')
+	plt.savefig('images/time_corr.pdf')
+	
