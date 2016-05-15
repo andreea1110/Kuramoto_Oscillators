a0224d01c64a33b9960f0b058085530fc17ea461
Andreea Ciuprina <andreea.c1110@gmail.com>
2016-05-15 12:06:58 +0200

    Implement reconstruction for any network size.

--- main.py
+++ main.py
@@ -28,49 +28,54 @@ def compute_phase_diffs(sol, t, Omega):
 		phi[i] = (Phi(t[-1]) % (2*np.pi)) - sol[i, -1]
 		if phi[i] < 0:
 			phi[i] += 2*np.pi
-
-	print("phi = ", phi)
 	return phi
 
 def reconstruct_coefs(t, Omega_vec, omega, A, B):
-	"""
+	""" Recontruct the coupling coefficients strored in matrices A and B, 
+		using the phase differences between the oscillators and the driver
+		obtained for each Omega in Omega_vec.
 	"""
 	nf = np.size(A,0)
-	print("Omega_vec = ", Omega_vec)
-	print("np.repeat(Omega_vec, nf) = ", np.repeat(Omega_vec, nf))
 	b = np.repeat(Omega_vec, nf) - omega[1]
+
+	print("Solving a system of {} equations and {} unknowns.".format(len(Omega_vec)*nf, nf**2))
 
 	M = np.zeros((len(Omega_vec)*nf, nf**2))
 	for idx, Omega in enumerate(Omega_vec):
 		Phi = lambda t: Omega*t
 		sol = solve_system(t, omega, A, B, Phi)
 		phi = compute_phase_diffs(sol, t, Omega)
-		# for i in range(nf):
-		# 	row = np.array([])
-		# 	for j in range(nf):
-		# 		if i == j:
-		# 			continue
-		# 		row = np.append(row, np.sin(phi[i] - phi[j]))
-		# 	row = np.append(row, np.sin(phi[i]))
-		# 	row = np.append(row, np.zeros(nf**2 - nf))
-		# 	A[i, :] = row
-		row0 = np.array([np.sin(phi[0] - phi[1]), np.sin(phi[0]), 0, 0])
-		row1 = np.array([0, 0, np.sin(phi[1] - phi[0]), np.sin(phi[1])])
-		M[idx*2, :] = row0
-		M[idx*2 + 1, :] = row1
+		for i in range(nf):
+			row = np.array([])
+			for j in range(nf):
+				if i == j:
+					continue
+				row = np.append(row, np.sin(phi[i] - phi[j]))
 
-	print("M = \n", M)
-	print("b = \n", b)
+			row = np.append(row, np.sin(phi[i]))
+			M[idx*2 + i, i*nf : i*nf + nf] = row
+
+	evals = np.linalg.eigvals(np.array(M).T.dot(M))
+	cn = abs(max(evals) / min(evals))
+	print('Condition number:', cn, np.log10(cn))
+	
 	x = np.linalg.lstsq(M, b)[0]
-	print(x)
 
-	Arec = np.array([[0, x[0]], [x[2],0]])
-	Brec = np.array([x[1], x[3]])
+	x = np.reshape(x, (nf, nf))
+
+	Brec = x[:, -1]
+
+	Ahelp = x[:, 0:nf-1]
+	Arec = np.zeros((nf, nf))
+	for i in range(np.size(Ahelp, 0)):
+		Arec[i, :] = np.insert(Ahelp[i, :], i, 0)
 
 	print("original A = \n", A)
 	print("reconstructed A = \n", Arec)
 	print("original B = \n", B)
 	print("reconstructed B = \n", Brec)
+
+	return (Arec, Brec)
 
 
 
@@ -88,6 +93,7 @@ def main():
 
 	# load network of oscillators
 	G = load_simple_network()
+
 	A = nx.adjacency_matrix(G).todense()
 	
 
@@ -99,25 +105,20 @@ def main():
 	# the parameters of the external driver
 	Omega = 0.3
 	Phi = lambda t: Omega*t
-	B = np.array([7, 0])
+	B = np.random.uniform(0, 5, size=nf)
+	#B = np.array([7, 0])
 	#B = [1.00465019, 1.4795114, 3.92223887, 3.99427941, 4.8777141, 2.59484127, 3.6070913, 4.51140533, 3.38070504, 3.65192458]
 	
 	
 	# solve the system and plot the time evolution and the corresponding correlation values
 	sol, corr_vals = solve_system(t, w, A, B, Phi, True)
 	#plot_time_corr(G, sol, corr_vals, Omega, t)
-	print(sol.shape)
-	print(t[-1])
-	print("theta_0 = ", sol[0, t[-1]/dt])
-	print("theta_1 = ", sol[1, t[-1]/dt])
-	print("Phi = ",Phi(t[-1]) % (2*np.pi))
 
 	phi = compute_phase_diffs(sol, t, Omega)
 	print(phi)
 
-	Omega_vec = np.array([0.3, 0.5, 1.3, 1.7, 2.0])
-	#Omega_vec = np.arange(0.1, 2.0, 0.1)
-	print(Omega_vec.shape)
+	#Omega_vec = np.array([0.3, 0.5, 1.3, 1.7, 2.0])
+	Omega_vec = np.linspace(0.3, 2.0, 40)
 	reconstruct_coefs(t, Omega_vec, w, A, B)
 
 	plt.figure()
